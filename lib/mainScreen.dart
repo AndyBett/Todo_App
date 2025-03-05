159386ea47752569349a4efae7eec0c3c640f6fe
Andy Bett <andybett99@gmail.com>
2025-03-05 20:15:53 +0300

    Initial commit: Set up project structure for To-Do app

---
+++ lib/mainScreen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class Mainscreen extends StatefulWidget {
+  const Mainscreen({super.key});
+
+  @override
+  State<Mainscreen> createState() => _MainscreenState();
+}
+
+class _MainscreenState extends State<Mainscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(),
+      appBar: AppBar(
+        title: Text('Todo App'),
+        centerTitle: true,
+        actions: [
+          InkWell(
+            onTap: () {
+              showModalBottomSheet(
+                  context: context,
+                  builder: (context) {
+                    return Container(
+                      height: 250,
+                    );
+                  });
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(Icons.add),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
